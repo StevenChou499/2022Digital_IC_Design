87ae1656502dc71d06bead30d53a7313aeba0d69
StevenChou499 <stevenchou499@gmail.com>
2023-03-09 15:20:55 +0800

    Complete the verilog code for comparator.

--- HW1/MMS_4num.v
+++ HW1/MMS_4num.v
@@ -5,10 +5,28 @@ input  [7:0] number0;
 input  [7:0] number1;
 input  [7:0] number2;
 input  [7:0] number3;
-output [7:0] result; 
+output reg [7:0] result; 
 
-/*
-	Write Your Design Here ~
-*/
+reg [7:0] compare_1, compare_2;
+
+always @(select, number0, number1, number2, number3) begin
+	if (select == 0) begin // find the maximum
+		compare_1 = (number0 > number1) ? number0 : number1;
+		compare_2 = (number2 > number3) ? number2 : number3;
+	end
+	else begin // find the minimum
+		compare_1 = (number0 > number1) ? number1 : number0;
+		compare_2 = (number2 > number3) ? number3 : number2;
+	end
+end
+
+always @(compare_1, compare_2) begin
+	if (select == 0) begin // find the maximum
+		result = (compare_1 > compare_2) ? compare_1 : compare_2;
+	end
+	else begin // find the minimum
+		result = (compare_1 > compare_2) ? compare_2 : compare_1;
+	end
+end
 
 endmodule
--- HW1/MMS_8num.v
+++ HW1/MMS_8num.v
@@ -10,10 +10,22 @@ input  [7:0] number4;
 input  [7:0] number5;
 input  [7:0] number6;
 input  [7:0] number7;
-output [7:0] result; 
+output reg [7:0] result; 
 
-/*
-	Write Your Design Here ~
-*/
+wire [7:0] compare_1, compare_2;
+	
+// call MMS_4num module	
+MMS_4num MMS_4num_1(.result(compare_1), .select(select), .number0(number0), .number1(number1), .number2(number2), .number3(number3));
+MMS_4num MMS_4num_2(.result(compare_2), .select(select), .number0(number4), .number1(number5), .number2(number6), .number3(number7));
+
+always @(select, compare_1, compare_2) begin
+
+	if (select == 0) begin // find the maximum
+		result = (compare_1 > compare_2) ? compare_1 : compare_2;
+	end
+	else begin // find the minimum
+		result = (compare_1 > compare_2) ? compare_2 : compare_1;
+	end
+end
 
 endmodule
